b3ed6a731a50137ced69e56ec48ace11e3df868b
MMangus <mjmangus16@yahoo.com>
2019-07-01 20:19:04 -0400

    added a bootstrap file upload form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FileUpload from "./components/FileUpload";
+
 import "./App.css";
 
 const App = () => (
@@ -8,6 +10,7 @@ const App = () => (
       <i className="fab fa-react" />
       React File Upload
     </h4>
+    <FileUpload />
   </div>
 );
 
