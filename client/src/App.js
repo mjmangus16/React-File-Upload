08cd5a1888e15f1d54533984d63a0b4818fbd09b
MMangus <mjmangus16@yahoo.com>
2019-07-01 20:12:26 -0400

    initial commit after fixing minor git issue for client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" />
+      React File Upload
+    </h4>
+  </div>
+);
+
+export default App;
---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FileUpload = () => {
+  return (
+    <>
+      <form />
+    </>
+  );
+};
+
+export default FileUpload;
